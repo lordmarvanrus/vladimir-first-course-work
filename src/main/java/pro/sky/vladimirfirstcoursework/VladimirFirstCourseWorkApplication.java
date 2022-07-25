a8afc3ca4f0a543ddba0e6a3513ba36d063f3f99
Vladimir <marvalo73@gmail.com>
2022-07-25 19:56:06 +0700

    Пустой коммит

---
+++ src/main/java/pro/sky/vladimirfirstcoursework/VladimirFirstCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.vladimirfirstcoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VladimirFirstCourseWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VladimirFirstCourseWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/vladimirfirstcoursework/VladimirFirstCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.vladimirfirstcoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VladimirFirstCourseWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
